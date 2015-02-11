206ed1df7d13db03114224c315699cc921deed9a
maliemin-Mstar <maliemin.jj@gmail.com>
2015-02-11 19:58:31 +0800

    modify

--- AerospikeSessionHandler.php
+++ AerospikeSessionHandler.php
@@ -22,20 +22,20 @@ class AerospikeSessionHandler implements \SessionHandlerInterface {
 	}
 
 	public function create_sid() {
-		$bins = array(
-			'placeholder' => 1,
-		);
 		$options = array(
 			Aerospike::OPT_POLICY_EXISTS => Aerospike::POLICY_EXISTS_CREATE,
 		);
 		do{
 			$session_id = mt_rand(0, 0xff) << 56 | mt_rand(0, 0xfffffff) << 28 | mt_rand(0, 0xfffffff);
 			$key = $this->_db->initKey($this->_namespace, $this->_set, $session_id);
+			$bins = array(
+				'created_at' => time(),
+			);
 			$status = $this->_db->put($key, $bins, $this->_ttl, $options);
 		}
 		while($status === Aerospike::ERR_RECORD_EXISTS);
 
-		$this->_session_data[$session_id] = '';
+		$this->_session_data[$session_id] = msgpack_pack($bins);
 
 		return sprintf('%016x', $session_id);
 	}
@@ -57,6 +57,9 @@ class AerospikeSessionHandler implements \SessionHandlerInterface {
 	}
 
 	public function read($session_id) {
+		if (!preg_match('/^[0-9a-f]{16}$/', $session_id))
+			throw new Duoshuo\Exception('不合法的session id', Duoshuo\Exception::MISSING_OR_INVALID_ARGUMENT);
+
 		$session_id = hexdec(substr($session_id, 0, 8)) << 32 | hexdec(substr($session_id, 8, 8));
 
 		if (isset($this->_session_data[$session_id]))
@@ -66,7 +69,15 @@ class AerospikeSessionHandler implements \SessionHandlerInterface {
 		$record = array();
 		$status = $this->_db->get($key, $record);
 
-		$session_data_array = $status === Aerospike::OK ? $record['bins'] : array();
+		if ($status === Aerospike::OK) {
+			$session_data_array = $record['bins'];
+		}
+		else {
+			$session_data_array = array(
+				'created_at' => time(),
+			);
+			$this->_db->put($key, $session_data_array, $this->_ttl);
+		}
 
 		return $this->_session_data[$session_id] = msgpack_pack($session_data_array);
 	}
